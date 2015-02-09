a4deabcfd5843a355a0ce41c64ff45120b72c5a1
Cesar Torres <cearto@berkeley.edu>
2015-02-09 10:31:23 -0800

    Changed name

--- app/hello_network.lua
+++ app/hello_network.lua
@@ -1,6 +1,6 @@
 require "cord" 
 
-local svc_manifest = {id=”ATeam”}
+local svc_manifest = {id="ApplesandBananas"}
 local msg = storm.mp.pack(svc_manifest)
 	storm.os.invokePeriodically(5*storm.os.SECOND, function()
 	storm.net.sendto(a_socket, msg, "ff02::1", 1525)
