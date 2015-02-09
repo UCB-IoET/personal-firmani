e3e6724455fce666340b981b107004e04e63e5be
Aparna Dhinakaran <aparnadhinak@berkeley.edu>
2015-02-09 10:26:42 -0800

    hello network

---
+++ app/hello_network.lua
@@ -0,0 +1,9 @@
+require "cord" 
+
+local svc_manifest = {id=”ATeam”}
+local msg = storm.mp.pack(svc_manifest)
+	storm.os.invokePeriodically(5*storm.os.SECOND, function()
+	storm.net.sendto(a_socket, msg, "ff02::1", 1525)
+end)
+
+cord.enter_loop()
