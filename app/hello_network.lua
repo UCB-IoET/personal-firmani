bcc783b78ee64e0aaf0c1c2daaae234e2959a2f9
Cesar Torres <cearto@berkeley.edu>
2015-02-09 10:37:47 -0800

    server code

--- app/hello_network.lua
+++ app/hello_network.lua
@@ -1,9 +1,19 @@
 require "cord" 
 
-local svc_manifest = {id="ApplesandBananas"}
-local msg = storm.mp.pack(svc_manifest)
-	storm.os.invokePeriodically(5*storm.os.SECOND, function()
-	storm.net.sendto(a_socket, msg, "ff02::1", 1525)
-end)
 
+-- create echo server as handler
+server = function()
+   sock = storm.net.udpsocket(1525, 
+			      function(payload, from, port)
+				 		print (string.format("from %s port %d: %s",from, port, payload))
+				 		
+				 		local svc_manifest = {id="ApplesandBananas"}
+						local msg = storm.mp.pack(svc_manifest)
+						storm.os.invokePeriodically(5 * storm.os.SECOND, function()
+								storm.net.sendto(sock, msg, "ff02::1", 1525)
+						end)
+			      end)
+end
+
+server()
 cord.enter_loop()
