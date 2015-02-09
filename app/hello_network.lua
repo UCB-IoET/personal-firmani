29531fb243d70446e5267f26e2604af6668f3b46
Cesar Torres <cearto@berkeley.edu>
2015-02-09 11:01:57 -0800

    Table broadcast and receive

--- app/hello_network.lua
+++ app/hello_network.lua
@@ -1,19 +1,51 @@
 require "cord" 
 
+services_heard = {}
 
 -- create echo server as handler
 server = function()
    sock = storm.net.udpsocket(1525, 
 			      function(payload, from, port)
-				 		print (string.format("from %s port %d: %s",from, port, payload))
-				 		
-				 		local svc_manifest = {id="ApplesandBananas"}
-						local msg = storm.mp.pack(svc_manifest)
-						storm.os.invokePeriodically(5 * storm.os.SECOND, function()
-								storm.net.sendto(sock, msg, "ff02::1", 1525)
-						end)
+				 		print (string.format("from %s port %d",from, port))
+				 		local msg = storm.mp.unpack(payload)
+				 		id = msg.id
+
+				 		if id then
+					 		print("id", msg.id)
+					 		if services_heard[id] then -- already seen
+					 			services_heard[id] = msg
+					 		else
+					 			table.insert(services_heard, msg)
+					 		end
+
+					 		table.insert(services_heard, msg)
+					 		for service, v in pairs(msg) do 
+					 			print(service)
+					 				for k, value in pairs(v) do
+					 					print("\t", k, ":",value)
+					 				end
+					 		end
+					 	end
 			      end)
+
+   -- BROADCAST
+   local svc_manifest = {id="ApplesandBananas"}
+	local msg = storm.mp.pack(svc_manifest)
+	storm.os.invokePeriodically(5 * storm.os.SECOND, function()
+			storm.net.sendto(sock, msg, "ff02::1", 1525)
+	end)
 end
 
 server()
+storm.os.invokePeriodically(5 * storm.os.SECOND, function()
+	print("\nCURRENT SERVICES HEARD")
+	for k, v in pairs(services_heard) do
+		print(k, "\n")
+		for i, j in pairs(v) do
+			print(i .. ":" .. j .. "\n")
+		end
+	end
+	print("\n------------------------")
+end)
+
 cord.enter_loop()
